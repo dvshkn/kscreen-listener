4fbb01325cd7b33a1ff467c0475626214217384b
David Knight <david@dvshkn.com>
2019-08-10 21:10:16 -0700

    track previous connection state, only run scripts on state change

--- signal_runner.py
+++ signal_runner.py
@@ -5,6 +5,10 @@ from pathlib import Path
 
 class SignalRunner:
 
+    STATE_UNKNOWN = -1
+    STATE_DISCONNECTED = 0
+    STATE_CONNECTED = 1
+
     def __init__(self, debounce_sec, display_name, cwd=Path.home()):
         self.timer = None
         self.debounce_sec = debounce_sec
@@ -13,6 +17,12 @@ class SignalRunner:
 
         self.connect_scripts = []
         self.disconnect_scripts = []
+
+        # TODO: The first state change after the script starts will always cause
+        #       scripts to run even if it's unnecessary. If the script knew how
+        #       to query the display connection state on startup it would
+        #       avoid this.
+        self.last_state = self.STATE_UNKNOWN
     
     def add_connect_script(self, path):
         self.connect_scripts.append(path)
@@ -36,13 +46,18 @@ class SignalRunner:
         display_dict = next(find_display)
 
         script_paths = None
+        new_state = None
         if display_dict['connected']:
             print(f'>>> {self.display_name} connected')
             script_paths = self.connect_scripts
+            new_state = self.STATE_CONNECTED
         else:
             print(f'>>> {self.display_name} disconnected')
             script_paths = self.disconnect_scripts
+            new_state = self.STATE_DISCONNECTED
 
-        for fpath in script_paths:
-            print(f'    running {fpath}')
-            subprocess.run(fpath, shell=True, cwd=self.cwd)
+        if new_state != self.last_state:
+            for fpath in script_paths:
+                print(f'    running {fpath}')
+                subprocess.run(fpath, shell=True, cwd=self.cwd)
+            self.last_state = new_state
